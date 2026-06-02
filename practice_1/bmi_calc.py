5e6386af5f46ea283c39c7cd262b2e1bf19b3720
Sina Ibn Amin <sinaibnamin@gmail.com>
2026-06-02 21:53:58 +0600

    ok

--- practice_1/bmi_calc.py
+++ practice_1/bmi_calc.py
@@ -5,11 +5,12 @@ bmi = weight / (height ** 2)
 
 print(f"\nYour BMI is: {bmi:.2f}")
 
-if bmi < 18.5:
-    print("Category: Underweight")
-elif bmi < 25:
-    print("Category: Normal")
-elif bmi < 30:
-    print("Category: Overweight")
-else:
-    print("Category: Obese")
+match bmi:
+    case _ if bmi < 18.5:
+        print("Category: Underweight")
+    case _ if bmi < 25:
+        print("Category: Normal")
+    case _ if bmi < 30:
+        print("Category: Overweight")
+    case _:
+        print("Category: Obese")
